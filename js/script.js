a9d1962ea2eba825c4257ff58c2df68f9530de22
Ali Ourrami <ourrami97@gmail.com>
2020-05-18 17:06:16 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+var typed = new Typed('.type', {
+    strings: ["i'm Ali Ourrami", "i'm Front-End And Back-End developer", "Welcome On My World"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
